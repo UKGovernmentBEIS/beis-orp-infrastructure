f9aaea55f04c7ec370c1f286b70d9cc9ca866fa8
Benjamin Sherwood <benjamin.sherwood@ext.ons.gov.uk>
2022-11-01 12:19:07 +0000

    Initial commit of basic TF structure, proves VPC creation in AWS

---
+++ terraform.tf
@@ -0,0 +1,35 @@
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+data "aws_availability_zones" "available" {}
+
+
+variable "assume_role" {
+  default = "ci"
+}
+
+variable "region" {
+  default = "eu-west-2"
+}
+
+
+terraform {
+  required_version = "~> 1.3.3"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "beis-dev-state"
+    key    = "beis-dev-state.tfstate"
+    region = "eu-west-2"
+  }
+}
+
+provider "aws" {
+  region = var.region
+  profile = "terraform"
+}
---
+++ test.tf
@@ -0,0 +1,4 @@
+# Create a test VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
