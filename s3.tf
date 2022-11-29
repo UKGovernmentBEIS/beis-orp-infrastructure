a9e430ebd102df484dab8362a94da44eb18b7a15
Benjamin Sherwood <benjaminsherwood@gmail.com>
2022-11-29 14:05:21 +0000

    Adding new bucket for uploads - Matt Whitfield

--- s3.tf
+++ s3.tf
@@ -6,3 +6,21 @@ resource "aws_s3_bucket_acl" "beis-orp-ingest" {
   bucket = aws_s3_bucket.beis-orp-ingest.id
   acl    = "private"
 }
+
+resource "aws_s3_bucket" "beis-orp-datalake" {
+  bucket = "beis-orp-${local.environment}-datalake"
+}
+
+resource "aws_s3_bucket_acl" "beis-orp-datalake" {
+  bucket = aws_s3_bucket.beis-orp-datalake.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket" "beis-orp-upload" {
+  bucket = "beis-orp-${local.environment}-upload"
+}
+
+resource "aws_s3_bucket_acl" "beis-orp-upload" {
+  bucket = aws_s3_bucket.beis-orp-upload.id
+  acl    = "private"
+}
