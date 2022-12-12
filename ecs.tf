c5cfad16aa95ac742fd01844ec6f3116109f80b4
mattwhitfield <whitfield.mj@gmail.com>
2022-12-12 16:26:11 +0000

    add api upload key environment variable for app

--- ecs.tf
+++ ecs.tf
@@ -70,6 +70,7 @@ resource "aws_ecs_task_definition" "webserver" {
       mc_api_key             = data.aws_secretsmanager_secret_version.mc_api_key.secret_string
       cloudwatch_group_name  = "/app/beis/client"
       cloudwatch_stream_name = local.environment
+      api_upload_key         = data.aws_secretsmanager_secret_version.api_upload_key.secret_string
     }
   )
 }
