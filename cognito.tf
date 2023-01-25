5713a1619df57c51fa10ca293285c38d616e2567
Benjamin Sherwood <benjaminsherwood@gmail.com>
2023-01-25 16:54:50 +0000

    Fixing email verification for Cognito

--- cognito.tf
+++ cognito.tf
@@ -8,6 +8,10 @@ resource "aws_cognito_user_pool" "beis" {
   user_attribute_update_settings {
     attributes_require_verification_before_update = ["email"]
   }
+
+  verification_message_template {
+    default_email_option = "CONFIRM_WITH_LINK"
+  }
 }
 
 resource "aws_cognito_user_pool_client" "beis_client" {
