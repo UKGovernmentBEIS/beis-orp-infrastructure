a3404f83338643e374d8efba59146b6a2408cff7
Ben.Jeffrey <ben.jeffrey@mdrx.tech>
2023-02-20 14:07:11 +0000

    Adding correct policies for newly created lambda's

--- iam.tf
+++ iam.tf
@@ -153,6 +153,57 @@ resource "aws_iam_policy" "pdf_to_text_lambda_s3_policy" {
   })
 }
 
+resource "aws_iam_policy" "docx_to_text_lambda_s3_policy" {
+  name        = "docx-to-text-Lambda-to-S3"
+  path        = "/"
+  description = "Allow "
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:ListBucket",
+          "s3:DeleteObject"
+        ],
+        "Resource" : [
+          "arn:aws:s3:::*/*",
+          aws_s3_bucket.beis-orp-datalake.arn,
+          aws_s3_bucket.beis-orp-upload.arn
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "title_generation_lambda_s3_policy" {
+  name        = "title-generation-Lambda-to-S3"
+  path        = "/"
+  description = "Allow "
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:ListBucket",
+          "s3:DeleteObject"
+        ],
+        "Resource" : [
+          "arn:aws:s3:::*/*",
+          aws_s3_bucket.beis-orp-datalake.arn,
+        ]
+      }
+    ]
+  })
+}
+
 resource "aws_iam_policy" "update_typedb_sqs_queue" {
   name        = "udpate-typedb-sqs-queue"
   path        = "/"
