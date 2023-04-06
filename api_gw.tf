45ce1ceb1ea96140250cb9d26415678a77d4d5b7
Ben.Jeffrey <ben.jeffrey@mdrx.tech>
2023-04-06 11:11:09 +0100

    Added headers and reponse body to the API

--- api_gw.tf
+++ api_gw.tf
@@ -18,6 +18,7 @@ resource "aws_api_gateway_method" "html_document_api_post" {
   resource_id   = aws_api_gateway_resource.html_document_api_resource.id
   http_method   = "POST"
   authorization = "NONE"
+  request_validator_id = aws_api_gateway_request_validator.html_document_api_validator.id
   request_parameters = {
     "method.request.header.Content-Type" = false
   }
@@ -26,16 +27,12 @@ resource "aws_api_gateway_method" "html_document_api_post" {
   }
 }
 
-resource "aws_api_gateway_integration" "html_document_api_lambda_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.private_rest_api.id
-  resource_id             = aws_api_gateway_resource.html_document_api_resource.id
-  http_method             = aws_api_gateway_method.html_document_api_post.http_method
-  integration_http_method = "POST"
-  type                    = "AWS"
-  uri                     = "arn:aws:apigateway:${local.region}:lambda:path/2015-03-31/functions/${module.html_trigger.lambda_function_arn}/invocations"
-  passthrough_behavior    = "WHEN_NO_TEMPLATES"
-  content_handling        = "CONVERT_TO_TEXT"
-  credentials = aws_iam_role.api_gateway_execution_role.arn
+resource "aws_api_gateway_request_validator" "html_document_api_validator" {
+  name = "html_document_api_validator"
+  rest_api_id = aws_api_gateway_rest_api.private_rest_api.id
+
+  validate_request_body = true
+  validate_request_parameters = false
 }
 
 resource "aws_api_gateway_model" "request_validator_model" {
@@ -80,6 +77,49 @@ resource "aws_api_gateway_model" "request_validator_model" {
     "required": ["uuid", "regulator_id", "user_id", "uri", "document_type", "status", "topics"],
     "additionalProperties": false
   })
+}
+
+resource "aws_api_gateway_integration" "html_document_api_lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.private_rest_api.id
+  resource_id             = aws_api_gateway_resource.html_document_api_resource.id
+  http_method             = aws_api_gateway_method.html_document_api_post.http_method
+  integration_http_method = "POST"
+  type                    = "AWS"
+  uri                     = "arn:aws:apigateway:${local.region}:lambda:path/2015-03-31/functions/${module.html_trigger.lambda_function_arn}/invocations"
+  passthrough_behavior    = "WHEN_NO_TEMPLATES"
+  content_handling        = "CONVERT_TO_TEXT"
+  credentials             = aws_iam_role.api_gateway_execution_role.arn
+}
+
+resource "aws_api_gateway_integration_response" "html_document_api_lambda_integration_response_200" {
+  rest_api_id = aws_api_gateway_rest_api.private_rest_api.id
+  resource_id = aws_api_gateway_resource.html_document_api_resource.id
+  http_method = aws_api_gateway_method.html_document_api_post.http_method
+  status_code = "200"
+  response_templates = {
+    "application/json" = ""
+  }
+  content_handling = "CONVERT_TO_TEXT"
+}
+
+resource "aws_api_gateway_method_response" "response_200" {
+  rest_api_id = aws_api_gateway_rest_api.private_rest_api.id
+  resource_id = aws_api_gateway_resource.html_document_api_resource.id
+  http_method = aws_api_gateway_method.html_document_api_post.http_method
+  status_code = "200"
+  response_models = {
+    "application/json" = aws_api_gateway_model.empty_response_model.name
+  }
+  response_parameters = {
+    "method.response.header.Content-Type" = true
+  }
+}
+
+resource "aws_api_gateway_model" "empty_response_model" {
+  rest_api_id = aws_api_gateway_rest_api.private_rest_api.id
+  name        = "emptyResponseModel"
+  content_type = "application/json"
+  schema = jsonencode({})
 }
 
 resource "aws_api_gateway_rest_api_policy" "html_document_api_policy_resource" {
