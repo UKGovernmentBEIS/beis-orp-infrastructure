12ebd63285015527c1732bb4aa7357046b10b646
Ben Jeffrey <ben.jeffrey@mdrx.tech>
2023-04-13 12:12:07 +0100

    Added EB schedule to trigger the update lambda

--- eb.tf
+++ eb.tf
@@ -21,3 +21,24 @@ resource "aws_cloudwatch_event_rule" "eb_trigger" {
 }
 EOF
 }
+
+resource "aws_cloudwatch_event_rule" "legislation_table_update_trigger" {
+  name        = "legislation_table_update_trigger"
+  description = "Event rule to trigger the legislation_table_update Lambda function"
+
+  schedule_expression = "cron(0 0 ? * MON *)"
+}
+
+resource "aws_cloudwatch_event_target" "legislation_table_update_target" {
+  rule      = aws_cloudwatch_event_rule.legislation_table_update_trigger.name
+  arn       = module.legislation_table_update.lambda_function_arn
+  target_id = "legislation_table_update_target"
+}
+
+resource "aws_lambda_permission" "event_bridge_permission" {
+  statement_id  = "AllowExecutionFromCloudWatchEvents"
+  action        = "lambda:InvokeFunction"
+  function_name = module.legislation_table_update.lambda_function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.legislation_table_update_trigger.arn
+}
