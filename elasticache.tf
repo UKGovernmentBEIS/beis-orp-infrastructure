32294320e2403d86867d81b0043db177a055d5e6
Benjamin Sherwood <benjaminsherwood@gmail.com>
2023-04-14 15:05:44 +0100

    Fixing param group

--- elasticache.tf
+++ elasticache.tf
@@ -3,7 +3,7 @@ resource "aws_elasticache_cluster" "users" {
   engine               = "redis"
   node_type            = "cache.m5.large"
   num_cache_nodes      = 1
-  parameter_group_name = "default.redis6.2"
+  parameter_group_name = "default.redis6.x"
   engine_version       = "6.2"
   port                 = 6379
 }
