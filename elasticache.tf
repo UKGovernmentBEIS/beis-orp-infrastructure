c7ca79921d179c98531d7f168c7c9f572d92d62e
Benjamin Sherwood <benjaminsherwood@gmail.com>
2023-04-17 13:02:08 +0100

    Adding Subnet Group for ElastiCache

--- elasticache.tf
+++ elasticache.tf
@@ -6,5 +6,6 @@ resource "aws_elasticache_cluster" "users" {
   parameter_group_name = "default.redis6.x"
   engine_version       = "6.2"
   port                 = 6379
-  security_group_ids = [aws_security_group.ec_cluster.id]
+  security_group_ids   = [aws_security_group.ec_cluster.id]
+  subnet_group_name    = aws_elasticache_subnet_group.ecs.name
 }
