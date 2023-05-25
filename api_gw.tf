b8b755ae3f05474ac32fb90c219ba565f17d4cdc
Ben.Jeffrey <ben.jeffrey@mdrx.tech>
2023-05-25 08:22:45 +0100

    Added Terraform resources for deletion endpoint:

--- api_gw.tf
+++ api_gw.tf
@@ -160,3 +160,89 @@ resource "aws_api_gateway_stage" "private_rest_api_stage" {
   stage_name    = var.environment
   description   = "${var.environment} stage of the private ORP pipeline REST API"
 }
+
+resource "aws_api_gateway_resource" "delete_document_resource" {
+  rest_api_id = aws_api_gateway_rest_api.private_rest_api.id
+  parent_id   = aws_api_gateway_rest_api.private_rest_api.root_resource_id
+  path_part   = "delete-document"
+}
+
+resource "aws_api_gateway_method" "document_delete" {
+  rest_api_id          = aws_api_gateway_rest_api.private_rest_api.id
+  resource_id          = aws_api_gateway_resource.delete_document_resource.id
+  http_method          = "DELETE"
+  authorization        = "NONE"
+  request_validator_id = aws_api_gateway_request_validator.delete_validator.id
+  request_parameters = {
+    "method.request.header.Content-Type" = false
+  }
+  request_models = {
+    "application/json" = aws_api_gateway_model.delete_validator_model.name
+  }
+}
+
+resource "aws_api_gateway_integration" "html_delete_lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.private_rest_api.id
+  resource_id             = aws_api_gateway_resource.delete_document_resource.id
+  http_method             = aws_api_gateway_method.document_delete.http_method
+  integration_http_method = "POST"
+  type                    = "AWS"
+  uri                     = "arn:aws:apigateway:${local.region}:lambda:path/2015-03-31/functions/${module.delete_document.lambda_function_arn}/invocations"
+  passthrough_behavior    = "WHEN_NO_TEMPLATES"
+  content_handling        = "CONVERT_TO_TEXT"
+  credentials             = aws_iam_role.api_gateway_execution_role.arn
+}
+
+resource "aws_api_gateway_integration_response" "html_delete_lambda_integration_response_200" {
+  rest_api_id = aws_api_gateway_rest_api.private_rest_api.id
+  resource_id = aws_api_gateway_resource.delete_document_resource.id
+  http_method = aws_api_gateway_method.document_delete.http_method
+  status_code = "200"
+  response_templates = {
+    "application/json" = ""
+  }
+  content_handling = "CONVERT_TO_TEXT"
+}
+
+resource "aws_api_gateway_method_response" "response_200_delete" {
+  rest_api_id = aws_api_gateway_rest_api.private_rest_api.id
+  resource_id = aws_api_gateway_resource.delete_document_resource.id
+  http_method = aws_api_gateway_method.document_delete.http_method
+  status_code = "200"
+  response_models = {
+    "application/json" = aws_api_gateway_model.empty_response_model.name
+  }
+  response_parameters = {
+    "method.response.header.Content-Type" = true
+  }
+}
+
+resource "aws_api_gateway_request_validator" "delete_validator" {
+  name        = "delete_validator"
+  rest_api_id = aws_api_gateway_rest_api.private_rest_api.id
+
+  validate_request_body       = true
+  validate_request_parameters = false
+}
+
+resource "aws_api_gateway_model" "delete_validator_model" {
+  rest_api_id  = aws_api_gateway_rest_api.private_rest_api.id
+  name         = "deleteValidator"
+  content_type = "application/json"
+  schema = jsonencode({
+    "$schema" : "http://json-schema.org/draft-04/schema#",
+    "title" : "Document Delete Request Model",
+    "type" : "object",
+    "properties" : {
+      "uuid" : {
+        "type" : "string",
+        "format" : "uuid"
+      },
+      "regulator_id" : {
+        "type" : "string"
+      }
+    }
+    "required" : ["uuid", "regulator_id"],
+    "additionalProperties" : false
+  })
+}
